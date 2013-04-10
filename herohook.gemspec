19ccf57e4eab0237a5708a05db805e4d7e4d0c72
Jason Fleetwood-Boldt <jason@datatravels.com>
2013-04-10 14:24:16 -0400

    adding back airbake to gem

--- herohook.gemspec
+++ herohook.gemspec
@@ -23,6 +23,7 @@ Gem::Specification.new do |s|
   s.add_dependency "sinatra", ">= 1.3.2"
   s.add_dependency "pony", "~> 1.4"
   s.add_dependency "honeybadger", "1.6.1"
+  s.add_dependency "airbrake", "~> 3.0.9"
   s.add_dependency "pivotal-tracker", "~> 0.4.1"
 
   s.description = <<description
---
+++ lib/herohook/airbrake.rb
@@ -0,0 +1,17 @@
+require 'airbrake_tasks'
+
+module Herohook
+  class Airbrake < Base
+    def perform
+      ::Airbrake.configure do |airbrake_config|
+        airbrake_config.api_key = config["api_key"]
+      end
+      ::AirbrakeTasks.deploy(
+        :rails_env => config["environment"],
+        :scm_revision => params[:head],
+        :scm_repository => config["repository_url"],
+        :local_username => params[:user]
+        )
+    end
+  end
+end
